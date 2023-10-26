870a4b6732271824f302c0f45a5097afea9676dd
Mitch Duncan <mitch@mitchduncan.com>
2023-10-26 13:31:57 -0700

    workspace

---
+++ workspace/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  cloud {
+    organization = "mitchduncan"
+    workspaces {
+      name = "aws-vm"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server1" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+}
+
+
